4d07e651e1201d385bb27b44233f98fe253a24c4
Maharshi184 <140582672+Maharshi184@users.noreply.github.com>
2025-09-21 16:10:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-m43aidfvh-maharshi184s-projects.vercel.app",
+    origin: "https://chat-omebw6gik-maharshi184s-projects.vercel.app",
     methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     credentials: true,
   })
@@ -46,5 +46,6 @@ server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
+
 
 
