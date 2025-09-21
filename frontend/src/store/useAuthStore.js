fa4eed004aec751669949e09db7620d2cf39c964
Maharshi184 <140582672+Maharshi184@users.noreply.github.com>
2025-09-21 15:38:18 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.VITE_APP_API_URL;
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -103,4 +103,5 @@ export const useAuthStore = create((set, get) => ({
     if (get().socket?.connected) get().socket.disconnect();
   },
 }));
+
 
